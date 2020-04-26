a82e1805401d41a0950fad30bcb13f091bb5202e
denis_savitsky <denis_savickii96@mail.ru>
2020-04-26 13:44:17 +0300

    minor mistakes fixed

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+
 
 class MainPage(BasePage):
 
@@ -9,11 +9,5 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link), 'Login link is not present'
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), 'Login link is not present'
 
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except NoSuchElementException:
-            return False
-        return True

891dde7f5cc5cedf61214cca1f785cb7dc4ff1f6
denis_savitsky <denis_savickii96@mail.ru>
2020-04-29 21:22:19 +0300

    mark need_review moved to the right test

--- test_product_page.py
+++ test_product_page.py
@@ -23,7 +23,7 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.compare_product_name()
     product_page.compare_price()
 
-@pytest.mark.need_review
+
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     product_page = ProductPage(browser, link)
@@ -55,7 +55,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)

9cb6819a7dc8dff564bd16a6fd27a85d7a158ca6
denis_savitsky <denis_savickii96@mail.ru>
2020-04-29 15:51:56 +0300

    mark added to conftest

--- conftest.py
+++ conftest.py
@@ -2,16 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
 
+
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='Chrome',
                      help='Choose browser: Chrome or Firefox')
     parser.addoption('--language', action='store', default='en',
                      help='Choose language: ru, en, es, etc.')
 
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "login_guest: mark test to login from main page"
+    )
+
 
 @pytest.fixture(scope='function')
 def browser(request):
-
     browser_name = request.config.getoption('browser_name')
     user_language = request.config.getoption('language')
 
@@ -33,10 +38,9 @@ def browser(request):
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        
+
         raise pytest.UsageError(f"--browser_name should be Chrome or Firefox, while your input is {browser_name}")
 
     yield browser
     print('\nquit browser...')
     browser.quit()
-
